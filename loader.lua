9fcd1782c89cd7f053ff9e60045fdc68e7d477f3
a65407112-boop <a65407112@gmail.com>
2026-08-27 11:57:44 +0200

    Switch stable entrypoint to v5

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- Null Protocol stable entrypoint
-local url = "https://raw.githubusercontent.com/a65407112-boop/FunCombatAssets/main/loader_fixed.lua?cb=" .. tostring(os.clock())
+local url = "https://raw.githubusercontent.com/a65407112-boop/FunCombatAssets/main/loader_v5.lua?cb=" .. tostring(os.clock())
 local src = game:HttpGet(url)
 local fn, err = loadstring(src, "NullProtocolStableEntrypoint")
 if not fn then
